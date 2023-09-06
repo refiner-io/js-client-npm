379f7854baa0e2e9976a2d3b41de52e2508e2fb3
Ben Mann <benmann89@gmail.com>
2023-09-06 11:36:54 +0200

    wip: add vue js example

---
+++ examples/vue-js/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <main>
+      Refiner — Vue.js demo
+    </main>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import _refiner from 'refiner-js';
+
+  onMounted(() => {
+    _refiner('setProject', 'XYZ-XYZ-XYZ-XYZ-XYZ');
+
+    _refiner('identifyUser', {
+        id: 'USER-ID-XYZ-123',
+        email: 'jane@awesome.com',
+        name: 'Jane Doe',
+      });
+
+      _refiner('showForm', 'XYZ-XYZ-XYZ-XYZ-XYZ', true);
+  })
+</script>
